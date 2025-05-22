bad4e4c58b8eaefd1ba95972c27d0b7565b3867e
Falode Tobi <thobbytosin11@gmail.com>
2025-05-22 10:01:55 +0100

    updated

--- models/appointment.model.ts
+++ models/appointment.model.ts
@@ -10,6 +10,7 @@ import {
 import { Patient } from "./patient.model";
 import { Doctor } from "./doctor.model";
 import { Transaction } from "./transaction.model";
+import { User } from "./user.model";
 
 export enum AppointmentStatus {
   PENDING = "Pending",
@@ -41,15 +42,15 @@ export class Appointment extends Model {
   })
   id!: string;
 
-  @ForeignKey(() => Patient)
+  @ForeignKey(() => User)
   @Column({
     type: DataType.UUID,
     allowNull: false,
   })
   patientId!: string;
 
-  @BelongsTo(() => Patient)
-  patient!: Patient;
+  @BelongsTo(() => User)
+  patient!: User;
 
   @ForeignKey(() => Doctor)
   @Column({
@@ -177,64 +178,3 @@ export class Appointment extends Model {
   })
   reminderSentCount!: number;
 }
-
-// import mongoose, { Document, Model, Schema } from "mongoose";
-
-// interface IAppointment extends Document {
-//   patientId: mongoose.Schema.Types.ObjectId;
-//   doctorId: mongoose.Schema.Types.ObjectId;
-//   date: Date;
-//   time: string;
-//   reason: string;
-//   status: "Pending" | "Confirmed" | "Cancelled" | "Completed";
-//   isPaid: boolean;
-//   paymentDetails?: {
-//     method: string; // e.g., "Credit Card", "PayPal"
-//     transactionId: mongoose.Schema.Types.ObjectId;
-//     amount: number;
-//   };
-//   notes?: string;
-//   createdAt: Date;
-//   updatedAt: Date;
-// }
-
-// const appointmentSchema: Schema<IAppointment> = new mongoose.Schema(
-//   {
-//     patientId: {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: "Patient",
-//       required: true,
-//     },
-//     doctorId: {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: "Doctor",
-//       required: true,
-//     },
-//     date: { type: Date, default: Date.now(), required: true },
-//     time: { type: String, required: true },
-//     reason: { type: String, required: true },
-//     status: {
-//       type: String,
-//       enum: ["Pending", "Confirmed", "Cancelled", "Completed"],
-//       default: "Pending",
-//     },
-//     isPaid: { type: Boolean, default: false },
-//     paymentDetails: {
-//       method: { type: String },
-//       transactionId: {
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: "Transaction",
-//       },
-//       amount: { type: Number },
-//       notes: { type: String },
-//     },
-//   },
-//   { timestamps: true }
-// );
-
-// const Appointment: Model<IAppointment> = mongoose.model(
-//   "Appointment",
-//   appointmentSchema
-// );
-
-// export default Appointment;
